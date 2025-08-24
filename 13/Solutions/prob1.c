b80f7f5de08fd4a3275d25bf55dbf2e0657fbd5a
Alex-42-AI <81178614+Alex-42-AI@users.noreply.github.com>
2025-08-24 04:21:21 +0300

    Update prob1.c

--- 13/Solutions/prob1.c
+++ 13/Solutions/prob1.c
@@ -2,8 +2,8 @@
 #include<unistd.h>
 #include<sys/types.h>
 int main() {
-        printf("%d %d\n", getpid(), getppid());
-        int e = execlp("ls", "ls", NULL);
-        printf("%d\n", e);
-        return 0;
+    printf("%d %d\n", getpid(), getppid());
+    int e = execlp("ls", "ls", NULL);
+    printf("%d\n", e);
+    return 0;
 }
