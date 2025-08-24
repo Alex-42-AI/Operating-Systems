68da4a74dd461106e70e10ca8d763d5d12de2187
Alex-42-AI <81178614+Alex-42-AI@users.noreply.github.com>
2025-08-24 04:20:41 +0300

    Update prob6.c

--- 12/Solutions/prob6.c
+++ 12/Solutions/prob6.c
@@ -2,33 +2,34 @@
 #include<fcntl.h>
 #include<string.h>
 int main(int argc, char *argv[]) {
-        if (argc != 3)
-                return 1;
-        char *text = argv[1];
-        int fd = open(argv[2], O_RDONLY);
-        if (fd == -1)
-                return 1;
-        int i = 0;
-        char line[81], c;
-        while (read(fd, &c, 1) == 1) {
-                if (c == '\n' || i > 79) {
-                        line[i] = '\0';
-                        if (strstr(line, text) != NULL) {
-                                close(fd);
-                                return 0;
-                        }
-                        i = 0;
-                }
-                else
-                        line[i++] = c;
-        }
-        if (i) {
-                line[i] = '\0';
-                if (strstr(line, text) != NULL) {
-                        close(fd);
-                        return 0;
-                }
-        }
-        close(fd);
+    if (argc != 3)
         return 1;
+    char *text = argv[1];
+    int fd = open(argv[2], O_RDONLY);
+    if (fd == -1)
+        return 1;
+    int i = 0;
+    char line[81], c;
+    while (read(fd, &c, 1) == 1) {
+        if (c == '\n' || i > 79) {
+            line[i] = '\0';
+            if (strstr(line, text) != NULL) {
+                close(fd);
+                return 0;
+            }
+            i = 0;
+        }
+        else
+            line[i++] = c;
+    }
+    if (i) {
+        line[i] = '\0';
+        if (strstr(line, text) != NULL) {
+            close(fd);
+            return 0;
+        }
+    }
+    close(fd);
+    return 1;
 }
+
