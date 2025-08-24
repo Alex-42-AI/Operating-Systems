7f62bf5e33fe291e9878bcc2e7a9e75eb0c1cbeb
Alex-42-AI <81178614+Alex-42-AI@users.noreply.github.com>
2025-08-24 04:21:56 +0300

    Update prob2.c

--- 13/Solutions/prob2.c
+++ 13/Solutions/prob2.c
@@ -2,7 +2,10 @@
 #include<unistd.h>
 #include<sys/types.h>
 int main() {
-        int f = fork();
-        printf("%d %d %d\n", getpid(), getppid(), f);
-        return 0;
+    int f = fork();
+    if (f == -1)
+        return 1;
+    printf("%d %d %d\n", getpid(), getppid(), f);
+    return 0;
 }
+
