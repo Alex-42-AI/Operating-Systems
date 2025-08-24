3ff2a3ff4dbf3ac2f9e39a0147bce0f2a2e8264c
Alex-42-AI <81178614+Alex-42-AI@users.noreply.github.com>
2025-08-24 04:20:01 +0300

    Update prob4.c

--- 12/Solutions/prob4.c
+++ 12/Solutions/prob4.c
@@ -1,19 +1,20 @@
 #include<stdio.h>
 #include<fcntl.h>
 int main(int argc, char *argv[]) {
-        int i;
-        for (i = 1; i < argc; i++) {
-                int fd = open(argv[i], O_RDONLY);
-                if (fd == -1)
-                        continue;
-                while (1) {
-                        char buf[1];
-                        int r = read(fd, buf, 1);
-                        if (!r || r == -1)
-                                break;
-                        write(1, buf, 1);
-                }
-                close(fd);
+    int i;
+    for (i = 1; i < argc; i++) {
+        int fd = open(argv[i], O_RDONLY);
+        if (fd == -1)
+            continue;
+        while (1) {
+            char buf[1];
+            int r = read(fd, buf, 1);
+            if (!r || r == -1)
+                break;
+            write(1, buf, 1);
         }
-        return 0;
+        close(fd);
+    }
+    return 0;
 }
+
