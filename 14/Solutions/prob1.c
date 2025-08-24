a50dca06cec38ab9d364432fa85a5ecc57c0de1b
Alex-42-AI <81178614+Alex-42-AI@users.noreply.github.com>
2025-08-24 04:25:42 +0300

    Update prob1.c

--- 14/Solutions/prob1.c
+++ 14/Solutions/prob1.c
@@ -4,22 +4,23 @@
 #include<stdio.h>
 #include<fcntl.h>
 int main(int argc, char* argv[]) {
-        if (argc < 3)
-                return 1;
-        int f = fork();
-        if (f == -1)
-                return 1;
-        if (!f) {
-                int i;
-                for (i = 1; i < argc; i++)
-                        argv[i - 1] = argv[i];
-                argv[argc - 1] = NULL;
-                int res = execvp(argv[0], argv);
-        }
-        else {
-                int status;
-                int w = wait(&status);
-                printf("%d %d\n", w, status);
-        }
-        return 0;
+    if (argc < 3)
+        return 1;
+    int f = fork();
+    if (f == -1)
+        return 1;
+    if (!f) {
+        int i;
+        for (i = 1; i < argc; i++)
+            argv[i - 1] = argv[i];
+        argv[argc - 1] = NULL;
+        int res = execvp(argv[0], argv);
+    }
+    else {
+        int status;
+        int w = wait(&status);
+        printf("%d %d\n", w, status);
+    }
+    return 0;
 }
+
