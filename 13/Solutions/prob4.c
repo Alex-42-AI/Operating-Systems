ac1a5644d6fcd535a27156b5afb0e585de316efc
Alex-42-AI <81178614+Alex-42-AI@users.noreply.github.com>
2025-08-24 04:23:04 +0300

    Update prob4.c

--- 13/Solutions/prob4.c
+++ 13/Solutions/prob4.c
@@ -2,16 +2,19 @@
 #include<unistd.h>
 #include<sys/types.h>
 int main() {
-        int f = fork();
-        if (f) {
-                int status;
-                int w = wait(&status);
-                printf("%d %d\n", status, w);
-        }
-        else {
-                int e = execl("cat", "cat", 0);
-                printf("%s, %d\n", "child", e);
-        }
-        printf("%d %d %d\n", getpid(), getppid(), f);
-        return 0;
+    int f = fork();
+    if (f == -1)
+        return 1;
+    if (f) {
+        int status;
+        int w = wait(&status);
+        printf("%d %d\n", status, w);
+    }
+    else {
+        int e = execl("cat", "cat", 0);
+        printf("%s, %d\n", "child", e);
+    }
+    printf("%d %d %d\n", getpid(), getppid(), f);
+    return 0;
 }
+
