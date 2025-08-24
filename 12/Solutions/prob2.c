7f502b506303d69bb6f978b35b93f7c96efe0c3b
Alex-42-AI <81178614+Alex-42-AI@users.noreply.github.com>
2025-08-24 04:18:08 +0300

    Update prob2.c

--- 12/Solutions/prob2.c
+++ 12/Solutions/prob2.c
@@ -3,21 +3,21 @@
 #include<sys/types.h>
 #include<sys/stat.h>
 int main(int argc, char *argv[]) {
-        if (argc != 2)
-                return 1;
-        int fd = open(argv[1], O_CREAT | O_WRONLY | O_TRUNC, 0777);
-        if (fd == -1) {
-                perror(argv[1]);
-                return 1;
-        }
-        while (1) {
-                char buf[1];
-                int r = read(0, buf, 1);
-                if (!r || r == -1)
-                        break;
-                write(1, buf, 1);
-                write(fd, buf, 1);
-        }
-        close(fd);
-        return 0;
+    if (argc != 2)
+        return 1;
+    int fd = open(argv[1], O_CREAT | O_WRONLY | O_TRUNC, 0777);
+    if (fd == -1) {
+        perror(argv[1]);
+        return 1;
+    }
+    while (1) {
+        char buf[1];
+        int r = read(0, buf, 1);
+        if (!r || r == -1)
+            break;
+        write(1, buf, 1);
+        write(fd, buf, 1);
+    }
+    close(fd);
+    return 0;
 }
