c4365b8ee25b70f9a6367ca1bd6fb1ed4d80d5c2
Alex-42-AI <81178614+Alex-42-AI@users.noreply.github.com>
2025-08-24 04:17:24 +0300

    Update prob1.c

--- 12/Solutions/prob1.c
+++ 12/Solutions/prob1.c
@@ -3,12 +3,12 @@
 #include<sys/types.h>
 #include<sys/stat.h>
 int main() {
-        while (1) {
-                char buf[1];
-                int r = read(0, buf, 1);
-                if (!r || r == -1)
-                        break;
-                printf("%c", buf[0]);
-        }
-        return 0;
+    while (1) {
+        char buf[1];
+        int r = read(0, buf, 1);
+        if (!r || r == -1)
+            break;
+        printf("%c", buf[0]);
+    }
+    return 0;
 }
