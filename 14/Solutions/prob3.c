13de0bbca95cf444f3f7ae3091c6c32c8b86970f
Alex-42-AI <81178614+Alex-42-AI@users.noreply.github.com>
2025-08-24 04:26:56 +0300

    Update prob3.c

--- 14/Solutions/prob3.c
+++ 14/Solutions/prob3.c
@@ -3,32 +3,32 @@
 #include<sys/types.h>
 #include<sys/wait.h>
 int main(int argc, char *argv[]) {
-        if (argc != 3)
-                return 1;
-        int f = fork();
-        if (f == -1)
-                return 1;
-        if (f) {
-                int status0;
-                wait(&status0);
-                if (WIFEXITED(status0) && WEXITSTATUS(status0)) {
-                        int f0 = fork();
-                        if (f0 == -1)
-                                return 1;
-                        if (!f0) {
-                                execlp(argv[2], argv[2], NULL);
-                                return 1;
-                        }
-                        int status1;
-                        wait(&status1);
-                        if (WIFEXITED(status1))
-                                return WEXITSTATUS(status1);
-                        return 1;
-                }
-                if (WIFEXITED(status0))
-                        return WEXITSTATUS(status0);
-                return 1;
-        }
-        execlp(argv[1], argv[1], NULL);
+    if (argc != 3)
         return 1;
+    int f = fork();
+    if (f == -1)
+        return 1;
+    if (f) {
+        int status0;
+        wait(&status0);
+        if (WIFEXITED(status0) && WEXITSTATUS(status0)) {
+            int f0 = fork();
+            if (f0 == -1)
+                return 1;
+            if (!f0) {
+                execlp(argv[2], argv[2], NULL);
+                return 1;
+            }
+            int status1;
+            wait(&status1);
+            if (WIFEXITED(status1))
+                return WEXITSTATUS(status1);
+            return 1;
+        }
+        if (WIFEXITED(status0))
+            return WEXITSTATUS(status0);
+        return 1;
+    }
+    execlp(argv[1], argv[1], NULL);
+    return 1;
 }
